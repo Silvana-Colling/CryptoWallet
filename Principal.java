ad7102b94a16f299d1373563b02d20f92018054d
Silvana <silvana.braatz65@gmail.com>
2026-05-19 20:21:47 -0300

    Estrutura Base da Carteira2

---
+++ Principal.java
@@ -0,0 +1,7 @@
+public static void main(String[] args){
+    Carteira minhaCarteira = new Carteira("Thiago");
+    minhaCarteira.depositar(0.5);
+    System.out.println("Saldo de Bitcoin: " + minhaCarteira.saldoBitcoin);
+
+    
+}
