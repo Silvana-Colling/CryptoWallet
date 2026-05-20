e6c5024787827d50d8fe06ac127bce28ac20768c
Silvana <silvana.braatz65@gmail.com>
2026-05-19 21:40:35 -0300

    Adiciona senha de acesso à carteira

--- Carteira.java
+++ Carteira.java
@@ -1,13 +1,16 @@
 public class Carteira {
     String dono;
     double saldoBitcoin;
+    String senhaAcesso;
 
     public Carteira(String dono){
         this.dono = dono;
         this.saldoBitcoin = 1000000.0;
+        this.senhaAcesso = "123456"; //inicializando a senhaAcesso
     }
 
     public void depositar(double qtde){
         this.saldoBitcoin += qtde;
     }
 }
+
