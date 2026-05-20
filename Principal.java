57383951b5c8c95cc1f52ab6869a5ff3cc0535a7
Silvana Colling <silvana.braatz65@gmail.com>
2026-05-19 21:55:05 -0300

    Update Principal.java
    Inclusão msg depósito feito

--- Principal.java
+++ Principal.java
@@ -2,6 +2,6 @@ public static void main(String[] args){
     Carteira minhaCarteira = new Carteira("Thiago");
     minhaCarteira.depositar(0.5);
     System.out.println("Saldo de Bitcoin: " + minhaCarteira.saldoBitcoin);
-
+    System.out.println("Depósito Feito!");
     
 }
